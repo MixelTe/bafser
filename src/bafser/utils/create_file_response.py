4905e74889833043129e89010da44ad21f7b4d9a
Mike <63655075+MixelTe@users.noreply.github.com>
2026-02-25 23:17:57 +0300

    v2.8.6

--- src/bafser/utils/create_file_response.py
+++ src/bafser/utils/create_file_response.py
@@ -1,13 +1,13 @@
 import os
+
 from flask import current_app, make_response, send_file
 
 
-def create_file_response(fpath: str, ftype: str, fname: str):
+def create_file_response(fpath: str, ftype: str, fname: str, as_attachment: bool = False):
     if not os.path.exists(fpath):
         return make_response("", 404)
     max_age = current_app.config["CACHE_MAX_AGE"]  # type: ignore
-    response = send_file(fpath)
+    response = send_file(fpath, download_name=fname, as_attachment=as_attachment)
     response.headers.set("Content-Type", ftype)
-    response.headers.set("Content-Disposition", "inline", filename=fname)
     response.headers.set("Cache-Control", f"public,max-age={max_age},immutable")
     return response
